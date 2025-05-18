695070bbd8af8e23d5779964b9762b7add2ee792
Numonov Sardorbek <numonovsardorbek@gmail.com>
2025-05-18 16:44:08 +0900

    Initial Commit

---
+++ webviewapp_ios/webviewapp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewapp_iosApp.swift
+//  webviewapp_ios
+//
+//  Created by Sardorbek Numonov on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct webviewapp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ webviewapp_iosTests/webviewapp_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  webviewapp_iosTests.swift
+//  webviewapp_iosTests
+//
+//  Created by Sardorbek Numonov on 5/18/25.
+//
+
+import Testing
+@testable import webviewapp_ios
+
+struct webviewapp_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
